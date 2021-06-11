008c84b7b914822409111fae6bbb2a08179092b0
Boday Alfaro <bohdan.shlikhutka@gmail.com>
2021-06-11 21:09:45 +0300

    App configurations for Win10

--- csc-manager.bat
+++ csc-manager.bat
@@ -1,5 +1,13 @@
 @ECHO OFF
 setlocal
-venv\Scripts\python csc-manager.py
+set KEYGEN_CORE=..\keygen-core\
+set UI_DEPENDENCIES=..\ui-dependencies-win\
+set PYTHONPATH=%KEYGEN_CORE%;%UI_DEPENDENCIES%;%PYTHONPATH%
+
+echo KEYGEN_CORE = %KEYGEN_CORE%
+echo UI_DEPENDENCIES = %UI_DEPENDENCIES%
+echo PYTHONPATH = %PYTHONPATH%
+
+python csc-manager.py
 endlocal
 pause
