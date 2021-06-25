acebe15a568cc1b13bc34ebe01ff9b5f88856e96
Shlikhutka.Bohdan <bohdan.shlikhutka@gmail.com>
2021-06-25 18:35:08 +0300

    Hotfix

--- csc-manager.bat
+++ csc-manager.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 setlocal
 set KEYGEN_CORE=..\keygen-core-0.1.2\
-set KEYGEN_PLUGINS=..\keygen-plugins-0.1.1\
+set KEYGEN_PLUGINS=..\keygen-plugins-0.1.2\
 set UI_DEPENDENCIES=..\ui-dependencies-win\
 set PYTHONPATH=%KEYGEN_CORE%;%KEYGEN_PLUGINS%;%UI_DEPENDENCIES%;%PYTHONPATH%
 
